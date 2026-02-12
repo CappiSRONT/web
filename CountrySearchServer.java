f032c7ea5d566209024ab74cc954977d7adae094
CappiSRONT <cappiincorporated@gmail.com>
2026-02-11 22:06:21 -0600

    Update CountrySearchServer.java
    Addition warning

--- CountrySearchServer.java
+++ CountrySearchServer.java
@@ -210,6 +210,10 @@ public class CountrySearchServer {
             "            color: #c7c7c7;\n" +
             "            font-size: 0.95em;\n" +
             "        }\n" +
+            "        .claus {\n" +
+            "            color: #000000;\n" +
+            "            font-size: 0.35em;\n" +
+            "        }\n" +  
             "    </style>\n" +
             "</head>\n" +
             "<body>\n" +
@@ -223,6 +227,7 @@ public class CountrySearchServer {
             "        </form>\n" +
             "        <p class='info'>Try searching: United States, Germany, Japan, Brazil; Or use abreviation like, ger, united, or braz.</p>\n" +
             "        <p class='info' style='margin-top: 10px;'>" + countries.size() + " countries current publicly loaded.</p>\n" +
+            "        <p class='claus' style='margin-top: 5px;'>Countries current publicly loaded.</p>\n" +
             "    </div>\n" +
             "</body>\n" +
             "</html>";
